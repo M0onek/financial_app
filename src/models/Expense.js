3fac9146093f7b2674cd0fce13cac29066e1e4e1
szymonk <szymon_krzywiecki@hotmail.com>
2020-09-04 10:19:05 +0200

    Add income and expense model

---
+++ src/models/Expense.js
@@ -0,0 +1,32 @@
+import Sequelize from 'sequelize';
+import db from '../config/database';
+
+const Expense = db.define('expense', {
+  expenseId: {
+    type: Sequelize.UUIDV4,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  accountId: {
+    type: Sequelize.UUIDV4,
+    references: 'accounts',
+    referencesKey: 'accountId',
+  },
+  amount: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    required: true,
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Date.now(),
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+  },
+});
+
+export default Expense;
---
+++ src/models/Income.js
@@ -0,0 +1,32 @@
+import Sequelize from 'sequelize';
+import db from '../config/database';
+
+const Income = db.define('income', {
+  incomeId: {
+    type: Sequelize.UUIDV4,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  accountId: {
+    type: Sequelize.UUIDV4,
+    references: 'accounts',
+    referencesKey: 'accountId',
+  },
+  amount: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    required: true,
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Date.now(),
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+  },
+});
+
+export default Income;
