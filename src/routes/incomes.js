f08f3e846f858ba865c0c7bff2099f764e2f79fa
M0onek <szymon.krzywiecki@outlook.com>
2020-09-10 14:29:54 +0200

    Add incomes CRUD

---
+++ src/routes/incomes.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import Income from '../models/Income';
+
+const router = express.Router();
+
+router.get('/users/:id/accounts/:accountId/incomes', async (req, res) => {
+  try {
+    const incomes = await Income.findAll({
+      where: {
+        accountId: req.params.accountId,
+      },
+    });
+    res.send(incomes).status(200);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get('/users/:id/accounts/:accountId/incomes/:incomeId', async (req, res) => {
+  try {
+    const income = await Income.findByPk(req.params.incomeId);
+    if (!income) res.status(404).send();
+    res.send(income).status(200);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post('/users/:id/accounts/:accountId/incomes', async (req, res) => {
+  try {
+    req.body.accountId = req.params.accountId;
+    const income = await Income.create(req.body);
+    res.status(201).send(income);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.patch('/users/:id/accounts/:accountId/incomes/:incomeId', async (req, res) => {
+  const updates = Object.keys(req.body);
+  try {
+    const income = await Income.findByPk(req.params.incomeId);
+    updates.forEach((update) => {
+      income[update] = req.body[update];
+    });
+    income.save();
+    res.status(200).send(income);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete('/users/:id/accounts/:accountId/incomes/:incomeId', async (req, res) => {
+  const { incomeId } = req.params;
+  try {
+    const income = await Income.findByPk(incomeId);
+
+    if (!income) res.status(404).send();
+
+    await Income.destroy({
+      where: {
+        incomeId,
+      },
+    });
+    res.status(200).send(income);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import db from './config/database';
 import userRoute from './routes/users';
 import accountRoute from './routes/accounts';
+import incomeRoute from './routes/incomes';
 
 //  Test DB
 db.authenticate()
@@ -24,6 +25,8 @@ app.get('/', (req, res) => {
 app.use('/users', jsonParser, userRoute);
 
 app.use('/', jsonParser, accountRoute);
+
+app.use('/', jsonParser, incomeRoute);
 
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!');
